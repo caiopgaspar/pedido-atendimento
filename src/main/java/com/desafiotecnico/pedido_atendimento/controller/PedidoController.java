4be05c30799405e1db38bacfde6faa8b73ee6bef
caiopgaspar <caio_gaspar@yahoo.com.br>
2026-06-03 15:50:52 -0300

    create: PedidoController

---
+++ src/main/java/com/desafiotecnico/pedido_atendimento/controller/PedidoController.java
@@ -0,0 +1,30 @@
+package com.desafiotecnico.pedido_atendimento.controller;
+
+import com.desafiotecnico.pedido_atendimento.dto.request.CreatePedidoRequest;
+import com.desafiotecnico.pedido_atendimento.dto.response.PedidoResponse;
+import com.desafiotecnico.pedido_atendimento.service.PedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/pedido")
+@RequiredArgsConstructor
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+//    public PedidoController(PedidoService pedidoService){
+//        this.pedidoService = pedidoService;
+//    }
+
+    @PostMapping
+    public ResponseEntity<PedidoResponse> createPedido (@RequestBody CreatePedidoRequest request) {
+
+        PedidoResponse response = pedidoService.createPedido(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+}
